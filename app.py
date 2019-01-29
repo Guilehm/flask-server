240b5ddfec3baf0b60c4009c1ade29560bb02c4d
Guilherme Luis <guile.hm@hotmail.com>
2019-01-28 23:02:23 -0200

    Create Post and Category model

--- app.py
+++ app.py
@@ -26,5 +26,29 @@ class User(db.Model):
         return f'<User {self.username}>'
 
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('users', lazy=True))
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+
 if __name__ == '__main__':
     app.run()
